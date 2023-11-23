2e1e261a1f2216879ab5bbef1745c3e1f1af5fc1
Анастасия Искоскина <aiskoskina@yandex.ru>
2023-11-23 14:10:05 +0300

    change settings.py

--- foodgram_project/foodgram_project/settings.py
+++ foodgram_project/foodgram_project/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
-DEBUG = os.getenv('DEBUG', True) == 'False'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '158.160.69.232,127.0.0.1,localhost,foody.ddns.net').split(',')
 
 INSTALLED_APPS = [
